078d016daa52daf1df8002b1e996c4de8baca14f
varun-ameta <92434524+varun-ameta@users.noreply.github.com>
2024-09-27 09:51:39 +0530

    Initial commit

--- src/app/model/interface/roles.ts
+++ src/app/model/interface/roles.ts
@@ -1,37 +1,37 @@
-export interface IRole{
-    roleId:number,
-    role:string
+export interface IRole {
+    roleId: number,
+    role: string
 }
 
-export interface IDesignation{
-    designationId:number,
-    designation:string
+export interface IDesignation {
+    designationId: number,
+    designation: string
 }
 
-export interface APIResponseModel{
-    message:string,
-    result:boolean,
-    data:any
+export interface APIResponseModel {
+    message: string,
+    result: boolean,
+    data: any
 }
 
-export interface Employee{
+export interface Employee {
     empName: string,
-    empId:string,
+    empId: string,
     empCode: string,
     empEmailId: string,
     empDesignation: string,
-    role: string    
+    role: string
 }
 
-export interface Project{
+export interface Project {
     empName: string,
     empId: number,
-    empCode:string ,
+    empCode: string,
     empEmailId: string,
-    empDesignation:string ,
-    projectName:string ,
-    startDate:Date,
-    expectedEndDate:Date ,
-    clientName:string,
-    clientProjectId:number
+    empDesignation: string,
+    projectName: string,
+    startDate: Date,
+    expectedEndDate: Date,
+    clientName: string,
+    clientProjectId: number
 }
